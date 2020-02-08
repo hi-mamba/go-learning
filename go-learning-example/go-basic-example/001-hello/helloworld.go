7ee50083297f2d735bd9b7bdb2b53b4f83dd8628
pan奎 <nbapankui@163.com>
2020-02-08 17:21:45 +0800

    add go  语言教程

--- go-learning-example/go-basic-example/001-hello/helloworld.go
+++ go-learning-example/go-basic-example/001-hello/helloworld.go
@@ -4,7 +4,10 @@ package main - 每一个go程序必须以 package name 开头.
 包的设计主要用来做代码隔离和代码可复用.
 这段程序里面的包名叫做 main
 */
-import "fmt"
+import (
+	"../math_example"
+	"fmt"
+)
 // import "fmt" - 导入fmt包用来在main函数中的输出文字到标准输出设备
 
 
@@ -19,4 +22,7 @@ The { and } indicate the start and end of the main function.
 func main() {
 	// fmt.Println("Hello World") - 使用fmt包中的Println用来输出文字到标准输出设备
 	fmt.Println("Hello World")
+	fmt.Println(math_example.Add(1,1))
+	fmt.Println(math_example.Sub(1,1))
+
 }
