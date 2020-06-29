98946d6478ee21aa6a0fb008bf916a4ac057bf39
mamba <nbapankui@163.com>
2020-06-29 08:26:26 +0800

    add mysql crud

--- go-learning-example/go-web-example/01、mysql-example/01、InitDB.go
+++ go-learning-example/go-web-example/01、mysql-example/01、InitDB.go
@@ -1,38 +1,44 @@
-package init
+package main
 
 import (
 	"database/sql"
 	"fmt"
-	"strings"
+	_ "github.com/go-sql-driver/mysql" // 主意这里
 )
 
 const (
-	userName = ""
-	password = ""
-	ip       = ""
-	port     = ""
-	dbName   = ""
+	userName = "root"
+	password = "root"
+	ip       = "127.0.0.1"
+	port     = "3306"
+	dbName   = "example"
 )
 
 //Db数据库连接池
-var DB *sql.DB
+var SqlDB *sql.DB
 
 //注意方法名大写，就是public
-func Init() {
+
+func InitDB() SqlDB.DB {
 	//构建连接："用户名:密码@tcp(IP:端口)/数据库?charset=utf8"
-	path := strings.Join([]string{userName, ":", password, "@tcp(", ip, ":", port, ")/", dbName, "?charset=utf8"}, "")
+	//path := strings.Join([]string{userName, ":", password, "@tcp(",ip, ":", port, ")/", dbName, "?charset=utf8"}, "")
 
 	//打开数据库,前者是驱动名，所以要导入： _ "github.com/go-sql-driver/mysql"
-	DB, _ = sql.Open("path", path)
-
+	//DB, _ = sql.Open("mysql", path)
+	var err error
+	SqlDB, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/example")
+	//DB, _ := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/example")
+	fmt.Println("----", err)
 	//设置数据库最大连接数
-	DB.SetConnMaxLifetime(100)
+	SqlDB.SetConnMaxLifetime(100)
 	//设置上数据库最大闲置连接数
-	DB.SetMaxIdleConns(10)
+	SqlDB.SetMaxIdleConns(10)
 	//验证连接
-	if err := DB.Ping(); err != nil {
-		fmt.Println("open database fail")
-		return
+	if err := SqlDB.Ping(); err != nil {
+		fmt.Println("opon database fail", err)
+		return sql.DB{}
 	}
 	fmt.Println("connect success")
+	//defer SqlDB.Close()
+	return sql.DB{};
 }
--- go-learning-example/go-web-example/01、mysql-example/02、crud_example.go
+++ go-learning-example/go-web-example/01、mysql-example/02、crud_example.go
@@ -1,21 +1,61 @@
-package init
+package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql" // 主意这里
+)
+
+var DB *sql.DB
+
 
 func InsertUser() bool {
-	tx, err = DB.Begin()
+	DB,_ = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/example")
+	tx, err := DB.Begin()
 	if err != nil {
 		fmt.Println("tx fail")
 		return false
 	}
 	//准备sql语句
-	stmt, err = tx.Prepare("")
-	if err != nil{
-		fmt.Println("Prepare fail")
+	stmt, err := tx.Prepare("INSERT INTO user (`name`, `age`) VALUES (?, ?)")
+	if err != nil {
+		fmt.Println("Prepare fail", err)
 		return false
 	}
-
+	//将参数传递到sql语句中并且执行
+	res, err := stmt.Exec("张三", 33)
+	if err != nil {
+		fmt.Println("Exec fail")
+		return false
+	}
+	//将事务提交
+	tx.Commit()
+	//获得上一个插入自增的id
+	fmt.Println(res.LastInsertId())
+	return true
 
 }
 
- 
+func SelectUser() {
+	//执行查询语句
+	rows, err := DB.Query("SELECT id,name from user ")
+	if err != nil{
+		fmt.Println("查询出错了")
+	}
+
+	//循环读取结果
+	for rows.Next(){
+		//将每一行的结果都赋值到一个user对象中
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		fmt.Printf("rows id = %d, value = %s", id, name)
+
+	}
+	//return users
+}
+
+func main()  {
+	InsertUser()
+	SelectUser()
+}
